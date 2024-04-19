083043b853e6a948e6d40c26050a42afed3e18c2
Vianney Stroebel <628818+vibl@users.noreply.github.com>
2024-04-19 17:11:37 +0200

    -

--- vibl/fetcher/index.js
+++ vibl/fetcher/index.js
@@ -68,8 +68,7 @@ async function onRecord(query) {
     const { output_table, output_jsonata } = endpoint;
     const results = await sendRequest(endpoint, query);
     const requestsTable = grist.getTable("Requests");
-    const requestId = await requestsTable.create({ fields: { success: false } });
-    console.log('requestId:', requestId)
+    const { id: requestId } = await requestsTable.create({ fields: { success: false } });
     const output = await transformResults(output_jsonata, results);
     const rows = output.map((row) => ({ ...row, request: id }));
     await insertRowsIntoOutputTable(output_table, rows);
